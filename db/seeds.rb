c3c1c55ae0a04563e03ec4b9f6377cf8b6458324
Alexander Schaap <schaapal@mcmaster.ca>
2016-01-17 05:08:39 -0500

    Updated seeds.rb to test Offerings table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Charity.create([{name: 'Fun Charity', description: 'Fun fun fun!'}])
-users = User.create([{ :name => 'Laura', :email => 'laura.mccrackin@gmail.com', :password_digest => 'password', :group => Charity.find(1)}])
+Company.create([{name: 'Moneymaking Business', description: 'More!'}])
+User.create([{ :name => 'Laura', :email => 'laura.mccrackin@gmail.com', :password_digest => 'password', :group => Charity.find(1)}])
+User.create([{ :name => 'The Boss', :email => 'boss@bossmail.com', :password_digest => '12345', :group => Company.find_by( name: 'Moneymaking Business') }])
+Offering.create([{:name => 'Free donuts', available: DateTime.parse("09/01/2009 17:00"), description: "from the boss", company_id_id: Company.find_by(name: 'Moneymaking Business')}])
